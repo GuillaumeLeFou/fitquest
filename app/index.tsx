4d816c83c032f31db6e6109ee30e30d36398102b
Guillaume Vanvilthoven <guillaumevvn@gmail.com>
2025-07-15 23:12:57 +0200

    Test branch

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,32 @@
 import PrimaryButton from "@/components/ui/PrimaryButton";
 import { useRouter } from "expo-router";
+import { doc, onSnapshot } from "firebase/firestore";
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 import { COLORS } from "../constants/theme";
+import { db } from "../firebase/firebaseConfig";
+import { useAuth } from "../hooks/useAuth";
 
 export default function Home() {
   const router = useRouter();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (!user) return;
+
+    const userRef = doc(db, "users", user.uid);
+
+    const unsubscribe = onSnapshot(userRef, (docSnapshot) => {
+      if (docSnapshot.exists()) {
+        console.log("Données utilisateur :", docSnapshot.data());
+      } else {
+        console.log("Aucune donnée trouvée pour cet utilisateur.");
+      }
+    });
+
+    return () => unsubscribe();
+  }, [user]);
+
   return (
     <View
       style={{
